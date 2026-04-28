e1e0208cfff6be05c938083dc917a71df37c24fd
Akshithreddy-source <akshithreddy297@gmail.com>
2026-04-28 19:07:36 +0530

    Add files via upload

---
+++ variables_ec2.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {}
+variable "private_subnets" {}
+variable "target_group_arn" {}
+variable "alb_sg" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "desired_capacity" {}
